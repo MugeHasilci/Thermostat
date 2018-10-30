05ae2ceec85c24b276fcb383dac499739fab10cb
muge <muge.hasilci@hotmail.com>
2018-10-30 16:23:42 +0000

    up and down functionality added

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,7 +5,19 @@
   beforeEach(function(){
     thermostat = new Thermostat();
   });
+  it('sets the temperature to 20 degrees', function(){
+   expect(thermostat.temperature).toEqual(20);
+ });
    it('starts at 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20)
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+  it('increase the temperature with an up function', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+  it('decrease the temperature with a down function', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
 });
